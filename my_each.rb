593aaf47e0908d685fc381e7d8c50ded45b4a334
Neil Seejoor <nvseejoor@gmail.com>
2018-02-07 22:50:16 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
   # code here
+  i = 0
+  while i < array.length
+    yield
+  end
+end
+
+my_each(array) do |word|
+  word
 end
